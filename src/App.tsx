e32c7a275778e5a90ca2edff6aa7b5622ce8b508
Alexsander Arellano <aalexsander@gmail.com>
2023-08-11 08:16:46 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isChatboxVisible, setChatboxVisible] = useState(false);
+
+  const toggleChatbox = () => {
+    setChatboxVisible(!isChatboxVisible);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button id="toggleChatbox" onClick={toggleChatbox}>
+        Chat
+      </button>
+      <div className={`chatbox ${isChatboxVisible ? 'visible' : ''}`} id="chatbox">
+        <div id="root"></div>
+          <iframe src="https://chatbot-joyous.vercel.app/" width="350" height="100%"></iframe>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
